854dfe2947a076124e744279bda65232588d1922
AL-SAFWAN <al-safwan.uddin@abehr.com>
2021-04-06 16:50:51 +0100

    user info header

--- components/ChatScreen.js
+++ components/ChatScreen.js
@@ -1,22 +1,63 @@
+import { Avatar, IconButton } from "@material-ui/core";
+import { AttachFile, MoreVert } from "@material-ui/icons";
 import { useRouter } from "next/router";
 import { useAuthState } from "react-firebase-hooks/auth";
-import styled from "styled-components"
+import styled from "styled-components";
 import { auth } from "../firebase";
 
-function ChatScreen({chat, messages}) {
-    const [user] = useAuthState(auth);
-    const router = useRouter();
-    return (
-        <Container>
-            <Header>
-
-            </Header>
-        </Container>
-    )
+function ChatScreen({ chat, messages }) {
+  const [user] = useAuthState(auth);
+  const router = useRouter();
+  return (
+    <Container>
+      <Header>
+        <Avatar />
+        <HeaderInformation>
+          <h3>Recipient Email</h3>
+          <p>Last seen ...</p>
+        </HeaderInformation>
+        <HeaderIcons>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </HeaderIcons>
+      </Header>
+    </Container>
+  );
 }
 
-export default ChatScreen
+export default ChatScreen;
 
-const Container = styled.div``
+const Container = styled.div``;
 
-const Header = styled.div``
+const Header = styled.div`
+position:sticky;
+background-color:white;
+z-index:100;
+top: 0;
+display: flex;
+padding:11px;
+height: 80px;
+align-items: center;
+border-bottom: 1px solid whitesmoke;
+`;
+
+const HeaderInformation = styled.div`
+margin-left: 15px;
+flex: 1;
+>h3{
+    margin-bottom:3px;
+
+}
+>p{
+    font-size: 14px;
+    color: grey;
+
+}
+
+`;
+
+const HeaderIcons = styled.div``;
