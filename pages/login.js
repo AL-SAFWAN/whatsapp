d8af43e5d80d0b69d8bdc9799c2ed2f9861bebcd
AL-SAFWAN <al-safwan.uddin@abehr.com>
2021-04-06 09:01:24 +0100

    add google auth popup

--- pages/login.js
+++ pages/login.js
@@ -1,18 +1,20 @@
 import { Button } from "@material-ui/core";
 import Head from "next/head";
 import styled from "styled-components";
+import { auth, provider } from "../firebase";
 
 function Login() {
+  const signIn =()=> {
+    auth.signInWithPopup(provider).catch(err => alert(err))
+  }
   return (
     <Container>
       <Head>
         <title>Login</title>
       </Head>
       <LoginContainer>
-        <Logo
-        src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg"
-        />
-        <Button variant ={'outlined'}>Sing In With Google</Button>
+        <Logo src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg" />
+        <Button onClick ={signIn}variant={"outlined"}>Sing In With Google</Button>
       </LoginContainer>
     </Container>
   );
@@ -21,18 +23,24 @@ function Login() {
 export default Login;
 
 const Container = styled.div`
-display: grid;
-place-items:center;
-height:100vh;`;
-
-const LoginContainer = styled.div`
-display: flex;
-flex-direction: column;
+  display: grid;
+  place-items: center;
+  height: 100vh;
+  background-color:whitesmoke;
 `;
 
-const Logo = styled.img`
-height:200px;
-width:200px;
-margin-bottom: 50px;
+const LoginContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 100px;
+  align-items: center;
+  background-color:white;
+  border-radius: 15px;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  `;
 
+const Logo = styled.img`
+  height: 200px;
+  width: 200px;
+  margin-bottom: 50px;
 `;
